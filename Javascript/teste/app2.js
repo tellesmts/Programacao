8f42e4c5e14b085d8185508d180c49e747d7b3b8
Xablau <piskoxablau@gmail.com>
2023-02-18 02:30:25 -0300

    Quarto chromeCommit

--- Javascript/teste/app2.js
+++ Javascript/teste/app2.js
@@ -3,7 +3,7 @@ const readline = require('readline').createInterface({
   output: process.stdout
 })
 
-readline.question('Digite o primeiro númeroo: ', num1 => {
+readline.question('Digite agora o primeiro número: ', num1 => {
   readline.question('Digite o segundo número: ', num2 => {
     const soma = Number(num1) + Number(num2);
     console.log(`A soma de ${num1} e ${num2} é igual a ${soma}.`);
